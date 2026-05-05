39ec2f22a4ef4c349d83ff829fd029692cf3a1bd
Roytol <roey.toledo@gmail.com>
2026-05-05 17:27:23 +0300

    feat: enable React Router v7 future flags for transition and splat path handling

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <ThemeProvider>
         <AuthProvider>
           <QueryClientProvider client={queryClientInstance}>
-            <Router>
+            <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
               <AppRoutes />
             </Router>
             <Toaster />
